287f575a9f20dcb33e97c0d7a0eeba3962d9044f
Divya <divyamani.shivanna@phyelements.com>
2023-08-30 15:17:01 +0530

    ExcelApplication from Vb.Net

---
+++ ExcelForm/Excel.vb
@@ -0,0 +1,69 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Public Class Generate_Excel
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Excel_btn.Click
+        Dim appXL As Excel.Application
+        Dim wbXl As Excel.Workbook
+        Dim shXL As Excel.Worksheet
+        Dim raXL As Excel.Range
+
+        appXL = CreateObject("Excel.Application")
+        appXL.Visible = True
+
+        wbXl = appXL.Workbooks.Add
+        shXL = wbXl.ActiveSheet
+
+        shXL.Cells(1, 1).Value = "FirstName"
+        shXL.Cells(1, 2).Value = "LastName"
+        shXL.Cells(1, 3).Value = "FullName"
+        shXL.Cells(1, 4).Value = "Specialization"
+
+        With shXL.Range("A1", "D1")
+            .Font.Bold = True
+            .VerticalAlignment = Excel.XlVAlign.xlVAlignCenter
+        End With
+
+        Dim Students(5, 2) As String
+        Students(0, 0) = "Vijet"
+        Students(0, 1) = "Girish"
+        Students(1, 0) = "Atharva"
+        Students(1, 1) = "Niharika"
+        Students(2, 0) = "Johny"
+        Students(2, 1) = "Joshna"
+        Students(3, 0) = "Ram"
+        Students(3, 1) = "Mythili"
+        Students(4, 0) = "Umme"
+        Students(4, 1) = "Ayman"
+
+        shXL.Range("A2", "B6").Value = Students
+
+        raXL = shXL.Range("C2", "C6")
+        raXL.Formula = "=A2 & "" "" & B2"
+
+        With shXL
+            .Cells(2, 4).Value = "Kannada"
+            .Cells(3, 4).Value = "Mathematics"
+            .Cells(4, 4).Value = "Computer Science"
+            .Cells(5, 4).Value = "Mathematics"
+            .Cells(6, 4).Value = "Biology"
+        End With
+
+        raXL = shXL.Range("A1", "D1")
+        raXL.EntireColumn.AutoFit()
+
+        appXL.Visible = "True"
+        appXL.UserControl = "True"
+
+        raXL = Nothing
+        shXL = Nothing
+        wbXl = Nothing
+        appXL.Quit()
+        appXL = Nothing
+        Exit Sub
+
+Err_Handler:
+        MsgBox(Err.Description, vbCritical, "Error: " & Err.Number)
+
+
+
+    End Sub
+End Class
---
+++ ExcelForm/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports Excel = Microsoft.Office.Interop.Excel
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Excel_btn.Click
+        Dim appXL As Excel.Application
+        Dim wbXl As Excel.Workbook
+        Dim shXL As Excel.Worksheet
+        Dim raXL As Excel.Range
+
+        ' Start Excel and get Application object.
+        appXL = CreateObject("Excel.Application")
+        appXL.Visible = True
+
+        ' Add a new workbook.
+        wbXl = appXL.Workbooks.Add
+        shXL = wbXl.ActiveSheet
+
+        ' Add table headers going cell by cell.
+        shXL.Cells(1, 1).Value = "First Name"
+        shXL.Cells(1, 2).Value = "Last Name"
+        shXL.Cells(1, 3).Value = "Full Name"
+        shXL.Cells(1, 4).Value = "Specialization"
+
+        ' Format A1:D1 as bold, vertical alignment = center.
+        With shXL.Range("A1", "D1")
+            .Font.Bold = True
+            .VerticalAlignment = Excel.XlVAlign.xlVAlignCenter
+        End With
+
+        ' Create an array to set multiple values at once.
+        Dim students(5, 2) As String
+        students(0, 0) = "Zara"
+        students(0, 1) = "Ali"
+        students(1, 0) = "Nuha"
+        students(1, 1) = "Ali"
+        students(2, 0) = "Arilia"
+        students(2, 1) = "RamKumar"
+        students(3, 0) = "Rita"
+        students(3, 1) = "Jones"
+        students(4, 0) = "Umme"
+        students(4, 1) = "Ayman"
+
+        ' Fill A2:B6 with an array of values (First and Last Names).
+        shXL.Range("A2", "B6").Value = students
+
+        ' Fill C2:C6 with a relative formula (=A2 & " " & B2).
+        raXL = shXL.Range("C2", "C6")
+        raXL.Formula = "=A2 & "" "" & B2"
+
+        ' Fill D2:D6 values.
+        With shXL
+            .Cells(2, 4).Value = "Biology"
+            .Cells(3, 4).Value = "Mathmematics"
+            .Cells(4, 4).Value = "Physics"
+            .Cells(5, 4).Value = "Mathmematics"
+            .Cells(6, 4).Value = "Arabic"
+        End With
+
+        ' AutoFit columns A:D.
+        raXL = shXL.Range("A1", "D1")
+        raXL.EntireColumn.AutoFit()
+
+        ' Make sure Excel is visible and give the user control
+        ' of Excel's lifetime.
+        appXL.Visible = True
+        appXL.UserControl = True
+
+        ' Release object references.
+        raXL = Nothing
+        shXL = Nothing
+        wbXl = Nothing
+        appXL.Quit()
+        appXL = Nothing
+        Exit Sub
+Err_Handler:
+        MsgBox(Err.Description, vbCritical, "Error: " & Err.Number)
+    End Sub
+End Class
